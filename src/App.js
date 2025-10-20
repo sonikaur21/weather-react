36ea29d8770636f3302d6c904521b6f4c9b43aa7
sonikaur21 <skaur1902@gmail.com>
2025-10-20 17:51:04 -0400

    worked on weather.js changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import WeatherSearch from "./WeatherSearch";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.css";
 export default function App() {
   return (
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Weather.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Weather() {
   return (
+    //
     <div className="Weather">
       <form>
         <div className="row">
@@ -11,10 +13,15 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="sumbit" value="search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -23,13 +30,19 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
-            alt="sun icon"
-          />{" "}
-          6*F
+          <div className="clearfix">
+            <img
+              src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
+              alt="sun icon"
+              className="float-left"
+            />{" "}
+            <span className="float-left">
+              <span className="temperature">6</span>
+              <span className="unit">*F</span>
+            </span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
