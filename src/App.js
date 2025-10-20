51b15d9302753ab6ac8aa31c11b43016dedf4afc
sonikaur21 <skaur1902@gmail.com>
2025-10-20 11:45:07 -0400

    added search engine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import WeatherSearch from "./WeatherSearch";
+import Weather from "./Weather";
+import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <h2>
-        <WeatherSearch />
-      </h2>{" "}
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/sonikaur21" target="_blank">
-          Sonia Kaur
-        </a>{" "}
-        is{" "}
-        <a href="https://github.com/sonikaur21/weather-react" target="_blank">
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a href="https://weatherreactprojectsonia.netlify.app/" target="_blank">
-          hosted on Netlify
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <h2>
+          <Weather />
+        </h2>{" "}
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/sonikaur21" target="_blank">
+            Sonia Kaur
+          </a>{" "}
+          is{" "}
+          <a href="https://github.com/sonikaur21/weather-react" target="_blank">
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weatherreactprojectsonia.netlify.app/"
+            target="_blank"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="sumbit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1> New york </h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://www.gstatic.com/weather/conditions/v1/svg/mostly_cloudy_day_light.svg"
+            alt="sun icon"
+          />{" "}
+          6*F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>precipation:15%</li>
+            <li>Humidity:72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
