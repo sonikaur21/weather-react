ee5ae98ce7f0ee76e2c3a944c3a281f9a2c7d897
sonikaur21 <skaur1902@gmail.com>
2025-10-23 17:24:55 -0400

    added missing items

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -6,34 +6,47 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
   useEffect(() => {
-    setForecast("");
-  }, [props.city]);
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     console.log(response.data);
     setForecast(response.data.daily);
     setLoaded(true);
   }
-  if (loaded) {
-    return (
-      <div className="WeatherForecast">
-        {" "}
-        <div className="row">
-          <div className="col">
-            {" "}
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
-        </div>
-      </div>
-    );
-  } else {
+  function load() {
     const apiKey = "73a00877081bd43422bdee0f3022beb5";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        {" "}
+        <div className="row">
+          {forecast.map(function (dayilyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  {" "}
+                  <WeatherForecastDay data={forecast[0]} />{" "}
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
     return null;
   }
 }
